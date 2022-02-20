778b5110c4de8d55db35fc9a3122717268a7b35a
Feridun Erbaş <feridunerbas@gmail.com>
2022-02-20 23:06:58 +0300

    Added error response to NewsRepositoryTests

--- MVVMUnitTestingTests/Tests/Data/News/Repository/NewsRemoteRepositoryTests.swift
+++ MVVMUnitTestingTests/Tests/Data/News/Repository/NewsRemoteRepositoryTests.swift
@@ -22,31 +22,43 @@ class NewsRemoteRepositoryTests: XCTestCase {
         }.implements(APIClient.self)
         sut = Resolver.fakes.optional()
     }
+    
+    override func tearDown() {
+        super.tearDown()
+        sut = nil
+        spy = nil
+    }
 }
 
+// MARK: - testHandlesEmptyData
 extension NewsRemoteRepositoryTests {
     
     func testHandlesEmptyData() {
         let expectation = XCTestExpectation()
+        // Given an unexpected(empty) data response
         spy.fakeResult = .success(Data())
+        // When getNews method called
         sut.getNews(request: .zero) { result in
             switch result {
             case .success:
                 break
             case .failure:
+                // Then repository should response with error
                 expectation.fulfill()
             }
         }
         wait(for: [expectation], timeout: 1)
     }
+}
+
+// MARK: - testHandlesGoodResponse
+extension NewsRemoteRepositoryTests {
     
     func testHandlesGoodResponse() {
         let expectation = XCTestExpectation()
-        
         // Given a json response consists of 2 News items
-        let filePath = R.file.mockNewsGoodResultWith2ItemsJson.path()!
+        let filePath = R.file.mockNewsGoodResponseWith2ItemsJson.path()!
         spy.responseFile(atPath: filePath)
-        
         // When getNews method called
         sut.getNews(request: .zero) { result in
             switch result {
@@ -70,5 +82,31 @@ extension NewsRemoteRepositoryTests {
         }
         wait(for: [expectation], timeout: 1)
      
+    }
+}
+
+// MARK: - testHandlesErrorResponse
+extension NewsRemoteRepositoryTests {
+    
+    func testHandlesErrorResponse() {
+        let expectation = XCTestExpectation()
+        // Given a json response of api Error
+        let filePath = R.file.mockedNewsErrorResponseJson.path()!
+        spy.responseFile(atPath: filePath)
+        // When getNews method called
+        sut.getNews(request: .zero) { result in
+            switch result {
+            case .success(let response):
+                // Then response should fill error related fields
+                XCTAssertNil(response.news)
+                XCTAssertNotNil(response.code)
+                XCTAssertNotNil(response.status)
+                XCTAssertNotNil(response.message)
+                expectation.fulfill()
+            case .failure:
+                break
+            }
+        }
+        wait(for: [expectation], timeout: 1)
     }
 }
